eeacf28904cf0518ce5ccc38af7fe6364010ca24
kfsofronas <kfsofronas@users.noreply.github.com>
2021-03-31 20:59:47 -0400

    Add _Scripts and Animation via upload

---
+++ _Scripts/throwHitting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class throwHitting : MonoBehaviour
+{
+
+	public bool hit = false;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		hit = true;
+		if (col.gameObject.tag.Equals("Player1") || col.gameObject.tag.Equals("P1ThrowHitbox")) { /*do nothing*/ }
+		else if (col.gameObject.tag.Equals("Player2") && !col.gameObject.GetComponent<Player2Controller>().invulnerable) { col.gameObject.GetComponent<Player2Controller>().being_thrown = true; print("collided with player 2"); /*this doesnt work somehow*/ gameObject.GetComponentInParent<PlayerController>().throw_connect = true; }
+		else if (col.gameObject.tag.Equals("P2ThrowHitbox")) { /*throw tech*/ gameObject.GetComponentInParent<PlayerController>().throwTech = true; print("TECHED THE THROW!1"); }
+	}
+}
